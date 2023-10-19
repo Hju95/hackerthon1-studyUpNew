87c8093d982aaceec052c121f21155e90c9bcb89
aacara <budgielover@naver.com>
2023-10-19 11:45:29 +0900

    Init project
    프로젝트 초기화

---
+++ src/main/java/com/kernel360/hackerthon/studyup/StudyupApplication.java
@@ -0,0 +1,13 @@
+package com.kernel360.hackerthon.studyup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kernel360/hackerthon/studyup/StudyupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kernel360.hackerthon.studyup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
