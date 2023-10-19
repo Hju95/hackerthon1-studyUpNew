f1b0c8fce267f3927ce5a3fe326546386a3bcc2a
aacara <budgielover@naver.com>
2023-10-20 08:40:10 +0900

    Fix 보완
    - application.properties의 db 개인정보 숨김처리
    - 페이지 폴더 디렉토리명 변경
      - view -> views

--- src/main/java/com/kernel360/hackerthon/studyup/common/controller/HomeController.java
+++ src/main/java/com/kernel360/hackerthon/studyup/common/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.kernel360.hackerthon.studyup.common.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
